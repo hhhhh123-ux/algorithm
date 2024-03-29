0ded604fa8f1c92f3a7eb7515bc0fb90611735a8
geff_gao@sottop.cn <13162308906>
2024-03-29 16:54:47 +0800

    算法-数组-回溯算法

--- src/main/java/com/gzq/demo/leetcode/数组/组合总和II/Solution.java
+++ src/main/java/com/gzq/demo/leetcode/数组/组合总和II/Solution.java
@@ -1,24 +1,26 @@
-package com.gzq.demo.leetcode.数组.组合总和;
+package com.gzq.demo.leetcode.数组.组合总和II;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
+
+
     List<List<Integer>> result = new ArrayList<>();
     LinkedList<Integer> temp = new LinkedList<>();
 
     public static void main(String[] args) {
-        System.out.println(new Solution().combinationSum(new int[]{8, 7, 4, 3}, 11));
+        System.out.println(new Solution().combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8));
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         backtracking(candidates, 0, target);
         return result;
     }
-
 
     void backtracking(int[] candidates, int begin, int target) {
         if (target == 0) {
@@ -29,10 +31,13 @@ public class Solution {
             if (target - candidates[i] < 0) {
                 break;
             }
+            // 小剪枝：同一层相同数值的结点，从第 2 个开始，候选数更少，结果一定发生重复，因此跳过，用 continue
+            if (i > begin && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
             temp.add(candidates[i]);
-            backtracking(candidates, i, target - candidates[i]);
+            backtracking(candidates, i + 1, target - candidates[i]);
             temp.removeLast();
         }
     }
-
 }
